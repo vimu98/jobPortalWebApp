9d8455114dbb285b8a270f413670d5e74ec0d0f9
Thimira Vimukthi <ictthimira@gmail.com>
2024-08-28 11:56:05 +0530

    Update constant.js

--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -1,5 +1,5 @@
 
-export const USER_API_END_POINT="https://job-portal-app-backend-859f13a9f2f5.herokuapp.com/api/v1/user";
-export const JOB_API_END_POINT="https://job-portal-app-backend-859f13a9f2f5.herokuapp.com/api/v1/job";
-export const APPLICATION_API_END_POINT="https://job-portal-app-backend-859f13a9f2f5.herokuapp.com/api/v1/application";
-export const COMPANY_API_END_POINT="https://job-portal-app-backend-859f13a9f2f5.herokuapp.com/api/v1/company";
+export const USER_API_END_POINT="https://job-portal-web-app-a26ba874efca.herokuapp.com/api/v1/user";
+export const JOB_API_END_POINT="https://job-portal-web-app-a26ba874efca.herokuapp.com/api/v1/job";
+export const APPLICATION_API_END_POINT="https://job-portal-web-app-a26ba874efca.herokuapp.com/api/v1/application";
+export const COMPANY_API_END_POINT="https://job-portal-web-app-a26ba874efca.herokuapp.com/api/v1/company";
